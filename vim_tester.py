211fe154ce97f9343fd997b079fed5fb62e59aee
ocros03 <ocros03@gmail.com>
2017-02-13 23:37:14 +0000

    made some changes

--- vim_tester.py
+++ vim_tester.py
@@ -1,39 +1,42 @@
 import random as rand
+import vim_list
 
-vim_list=[
-['h', 'moves one character to the left'],
-['l', 'moves one character to the right']
-]
+def main():
 
-print('Welcome to VIM tester!')
-print('What is your name?')
+	print('Welcome to VIM tester!')
+	print('What is your name?')
 
-name=input()
+	name=input()
 
-print('Hello, ' +name)
-print('I am going to ask you a series of questions to test your VIM knowledge')
+	print('Hello, ' +name)
+	print('I am going to ask you a series of questions to test your VIM knowledge')
 
-flag=1
-right=0
-wrong=0
+	flag=1
+	right=0
+	wrong=0
 
-while flag == 1:
-	n=rand.randint(0,1)
-	print('What command ' +vim_list[n][1]+ '?')
-	answer=input()
-	if answer==vim_list[n][0]:
-		print('Yes, you are correct!')
-		right=right+1
+	the_list = vim_list.commands
+
+	while flag == 1:
+		n=rand.randint(0,1)
+		print('What command ' +the_list[n][1]+ '?')
+		answer=input()
+		if answer==the_list[n][0]:
+			print('Yes, you are correct!')
+			right=right+1
+		else:
+			print('Not quite.  The answer is ' +the_list[n][0])
+			wrong=wrong+1
+		print("Enter 1 to continue, or 0 to quit")
+		flag=int(input())
+
+	total=right+wrong
+	percentage=round(right/total*100,1)
+	print(name+ ', you got ' +str(percentage)+ '% correct.')
+	if percentage >= 80:
+		print(name+ ', you are a VIM master!')
 	else:
-		print('Not quite.  The answer is ' +vim_list[n][0])
-		wrong=wrong+1
-	print("Enter 1 to continue")
-	flag=int(input())
+		print('Keep working on it, ' +name+ '!') 
 
-total=right+wrong
-percentage=right/total*100
-print(name+ ' you got ' +str(percentage)+ '% correct.')
-if percentage >= 80:
-	print('You are a VIM master!')
-else:
-	print('Keep working on it!') 
+if __name__ == '__main__':
+	main()

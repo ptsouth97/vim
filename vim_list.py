9763db2b6e769f4290f5a3287f9870293c00a12a
ocros03 <ocros03@gmail.com>
2017-02-14 02:00:37 +0000

    command list update

--- vim_list.py
+++ vim_list.py
@@ -1,5 +1,10 @@
-
 commands=[
 ['h', 'moves one character to the left'],
-['l', 'moves one character to the right']
+['l', 'moves one character to the right'],
+['j', 'moves one character down'],
+['k', 'moves one character up'],
+['0', 'moves to the start of the line'],
+['$', 'moves to the end of the line'],
+['w', 'moves one word forward (with punctuation)'],
+['2w', 'moves two words forward']
 ]
